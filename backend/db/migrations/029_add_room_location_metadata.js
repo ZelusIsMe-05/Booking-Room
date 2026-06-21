12a640f9efb23430f77139922d2a16ab6a0cb638
Zelus120805 <dldthinh1208@gmail.com>
2026-06-21 21:30:47 +0700

    Add 029_add_room_location_metadata.js

---
+++ backend/db/migrations/029_add_room_location_metadata.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  // Code sẽ được cập nhật sau bởi đồng đội
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  // Code sẽ được cập nhật sau bởi đồng đội
+};
